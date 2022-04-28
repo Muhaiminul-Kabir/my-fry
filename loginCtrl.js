5461cf8698bcfe731a3384316582804450ca28e2
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-04-28 13:39:41 +0600

    hdkdfhf

--- loginCtrl.js
+++ loginCtrl.js
@@ -6,19 +6,24 @@ import { isExist } from "./server.js";
 let lginbtn = document.getElementById('login');
 
 let name = document.getElementById('uid').value;
-let pass2 = document.getElementById('upass').value 
+let pass2 = document.getElementById('upass').value;
 
+window.onload = (event) => {
+    lginbtn = document.getElementById('login');
 
+    name = document.getElementById('uid').value;
+    pass2 = document.getElementById('upass').value
+};
 const user = {
     username: name,
     pass: pass2
-  };
-
-  
-
-lginbtn.onclick = function(){isExist(user.username)};
+};
 
 
 
+lginbtn.onclick = function () { isExist(user.username) };
 
-  
+
+
+
+
