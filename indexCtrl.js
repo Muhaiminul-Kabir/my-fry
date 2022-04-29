0719c6641bc467b69fa26e7fd2c0a62758881041
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-04-29 11:49:52 +0600

    signup init

--- indexCtrl.js
+++ indexCtrl.js
@@ -3,6 +3,8 @@ import { isExist } from "./server.js";
 
 let lginbtn = document.getElementById('login');
 
+let supbtn = document.getElementById('sup');
+
 
 let name = document.getElementById('uid');
 let pass2 = "ihggugugju";
@@ -18,6 +20,10 @@ document.getElementById('h').onload = function () { ingit() };
 lginbtn.onclick = function () {
     let id = document.getElementById('uid').value;
     isExist(id);
+};
+
+supbtn.onclick = function () {
+    window.location.href = "signup.html";
 };
 
 function ingit() {
