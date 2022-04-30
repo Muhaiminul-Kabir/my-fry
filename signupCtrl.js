9a4c66d74253277bf840d354be8409e3ddb2f9ad
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-04-30 13:43:10 +0600

    yuggdw

--- signupCtrl.js
+++ signupCtrl.js
@@ -1,4 +1,4 @@
-import { parseIp,isExist, isIn, writeUserData, getIp } from "./server.js";
+/*import { parseIp,isExist, isIn, writeUserData, getIp } from "./server.js";
 
 let supbtn = document.getElementById('signup');
 
@@ -29,4 +29,43 @@ supbtn.onclick = function () {
 
 
 
-};
+};*/
+
+
+
+function ip2int(ip) {
+    return (
+      ip.split(".").reduce(function(ipInt, octet) {
+        return (ipInt << 8) + parseInt(octet, 10);
+      }, 0) >>> 0
+    );
+  }
+  
+  const int2ip = ipInt => {
+    return (
+      (ipInt >>> 24) +
+      "." +
+      ((ipInt >> 16) & 255) +
+      "." +
+      ((ipInt >> 8) & 255) +
+      "." +
+      (ipInt & 255)
+    );
+  };
+  
+  const original_int = -1062731546;
+  
+  console.log("original_int", original_int);
+  
+  const ip = int2ip(original_int);
+  
+  console.log("converted ip", ip);
+  
+  const num = ip2int(ip);
+  
+  console.log("converted int", num);
+  
+  const ip2 = int2ip(num);
+  
+  console.log("converted ip", ip2);
+  
