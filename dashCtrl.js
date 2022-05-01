7e2dda106e1f80df2680292127b209cf202d80f0
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-05-01 14:14:30 +0600

    vsg

--- dashCtrl.js
+++ dashCtrl.js
@@ -1,14 +1,6 @@
 import { IP,ipInUse,getIp } from "./server.js"
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
-
 document.getElementById('h').onload = function(){
-    
+    getIp();
     ipInUse(IP);
 }
