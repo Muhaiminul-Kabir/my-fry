183cac44764646bc79cd011e9c2ea4f8486cba00
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-05-01 14:41:35 +0600

    fhdgf

--- dashCtrl.js
+++ dashCtrl.js
@@ -5,6 +5,6 @@ import { IP,ipInUse,getIP } from "./server.js"
 
 
 document.getElementById('h').onload = function(){
-   console.log(typeof getIP());
-   ipInUse(getIP);
+  
+   ipInUse(getIP());
 }
