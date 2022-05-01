e61bf643bb1c082ef3abbf47d4c1658988f91dc5
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-05-01 14:10:14 +0600

    g

--- dashCtrl.js
+++ dashCtrl.js
@@ -2,5 +2,6 @@ import { ipInUse,getIp } from "./server.js"
 
 document.getElementById('h').onload = function(){
     getIp();
+    sleep(2000);
     ipInUse(document.getElementById('uip').textContent);
 }
