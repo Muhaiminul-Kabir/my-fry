baa4814a796bf75e7b21852f387fc2f3d2c953f0
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-05-01 14:20:10 +0600

    vdvd

--- dashCtrl.js
+++ dashCtrl.js
@@ -2,6 +2,13 @@ import { IP,ipInUse,getIp } from "./server.js"
 
 
 
+document.getElementById('uip').onload = function(){
+    
+    let ip = document.getElementById('uip').textContent;
+    ipInUse(ip);
+}
+
+
 document.getElementById('h').onload = function(){
     
     let ip = document.getElementById('uip').textContent;
