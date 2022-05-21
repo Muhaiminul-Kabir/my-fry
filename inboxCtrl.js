39cbea1259188d5e33b439caac5a80ecc371d842
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-05-21 21:59:12 +0600

    scrolll

--- inboxCtrl.js
+++ inboxCtrl.js
@@ -1,8 +1,8 @@
 document.getElementById('sent-btn').onclick = function () {
-        var chatHistory = document.getElementById("chat-win");
+        var chatHistory = document.getElementById('chat-win');
         chatHistory.scrollTop = chatHistory.scrollHeight;
-        document.getElementById('chat-win').innerHTML += '<p style="max-width:200px;word-wrap:break-word;">'
-                + document.getElementById('msg').value
+        document.getElementById('chat-win').innerHTML += '<p style="color:white;font-weight: bold;;font-size:18px">[user]</p><p style="max-width:200px;word-wrap:break-word;">'
+                + document.getElementById('msg').value+'<br><p style="font-size:12px">'+new Date()+"</p></br>"
                 + '</p>';
 
 };
