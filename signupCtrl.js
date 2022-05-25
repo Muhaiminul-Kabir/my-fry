84bbd6c966a1967b0cadcd740170b90fd1bc62d5
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-05-25 12:57:56 +0600

    fgh

--- signupCtrl.js
+++ signupCtrl.js
@@ -16,6 +16,7 @@ supbtn.onclick = function () {
         if (pass == confirmPass) {
             console.log(document.getElementById('uip').textContent);
             writeUserData(id, pass);
+            window.location.href = "index.html";
         } else {
             console.log("didn't match");
             alert("Password doesn't match");
