06b7af2a7c10e53da192ef1dd2e7be823f63044c
NXN-pro <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2022-05-25 09:24:47 +0600

    drwds

--- mainAppCtrl.js
+++ mainAppCtrl.js
@@ -8,12 +8,11 @@ import { pubDicRead,pubDicWrite } from "./server.js";
 
 document.getElementById('sent-btn').onclick = function () {
         var user = sessionStorage.getItem("currentUser");
-        var chatHistory = document.getElementById('chat-win');
         var time = new Date();
-        chatHistory.scrollTop = chatHistory.scrollHeight;
-        document.getElementById('chat-win').innerHTML += '<p style="color:#66ff00;font-weight: bold;;font-size:18px">['+user+']</p><p style="max-width:200px;word-wrap:break-word;">'
+        
+        /*document.getElementById('chat-win').innerHTML += '<p style="color:#66ff00;font-weight: bold;;font-size:18px">['+user+']</p><p style="max-width:200px;word-wrap:break-word;">'
                 + document.getElementById('msg').value+'<br><p style="font-size:12px">'+time+"</p></br>"
-                + '</p>';
+                + '</p>';*/
         pubDicWrite(document.getElementById('msg').value,new Date());
 };
 
